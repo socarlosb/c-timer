72806ae1e52e40aa37c57c7342106588dcb62d48
Carlos <socarlosb@gmail.com>
2019-05-10 14:45:11 +0100

    added notification sound and vibration

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,35 @@
 <script>
+  import Switch from "./Switch.svelte";
   let timerDisplay = "25:00";
   let timerLeftDisplay = "-";
   let isPause = true;
   let initialTime = 25 * 60;
   let countdown;
   let secondsLeft = 0;
+  const audio = new Audio("./sound/this-guitar.mp3");
+  const vibrate =
+    navigator.vibrate ||
+    navigator.vibrate ||
+    navigator.webkitVibrate ||
+    navigator.mozVibrate ||
+    navigator.msVibrate;
 
+  let isSound = true;
+  let isVibrate = false;
+
+  function endCounter() {
+    if (isSound) {
+      console.log('is playing sound')
+      audio.play();
+    }
+
+    if (isVibrate) {
+      console.log('is vibrating')
+      navigator.vibrate(500);
+    }
+  }
   function timer(seconds) {
+    // console.log("isSound", isSound);
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
@@ -16,8 +39,9 @@
       secondsLeft = Math.round((then - Date.now()) / 1000);
       if (!isPause) {
         if (secondsLeft < 0) {
+          endCounter();
           clearInterval(countdown);
-          secondsLeft = 0
+          secondsLeft = 0;
           return;
         }
 
@@ -40,9 +64,18 @@
     const end = new Date(timestamp);
     const hour = end.getHours();
     const minutes = end.getMinutes();
-    timerLeftDisplay = `Ends at ${hour}:${
-      minutes < 10 ? "0" : ""
-    }${minutes}`;
+    timerLeftDisplay = `Ends at ${hour}:${minutes < 10 ? "0" : ""}${minutes}`;
+  }
+  function handleToggle(toggle) {
+    // console.log("in handleToggle");
+    // console.log("toggle", toggle);
+    if (toggle.name === "isSound") {
+      isSound = !toggle.val;
+      // console.log("toggle.name", isSound);
+    } else if (toggle.name === "isVibrate") {
+      isVibrate = !toggle.val;
+      // console.log("toggle.name", isVibrate);
+    } else console.log("not found");
   }
 
   const setState = type => {
@@ -63,7 +96,7 @@
 
       case "start":
         clearInterval(countdown);
-        
+
         isPause = false;
         if (secondsLeft) {
           timer(secondsLeft);
@@ -73,6 +106,7 @@
         break;
 
       case "restart":
+        isPause = true;
         clearInterval(countdown);
         secondsLeft = 0;
         timerDisplay = "25:00";
@@ -100,21 +134,21 @@
     padding: 1em;
     border-radius: 5px;
   }
-  .container i{
-    color: #fff; 
+  .container i {
+    color: #fff;
   }
   .time {
     text-align: center;
     font-size: 4em;
   }
-  .time--left{
+  .time--left {
     text-align: center;
     font-size: 2em;
-
   }
   .controls {
     display: flex;
     justify-content: space-around;
+    margin-bottom: 1.5em;
   }
   button {
     padding: 1em 2em;
@@ -123,7 +157,7 @@
     border: #fff solid 1px;
     cursor: pointer;
   }
-  button:disabled{
+  button:disabled {
     background-color: rgba(157, 202, 238, 0.5);
     cursor: inherit;
   }
@@ -134,13 +168,22 @@
     <h2 class="time">{timerDisplay}</h2>
     <h2 class="time--left">{timerLeftDisplay}</h2>
     <div class="controls">
+      <Switch toggle={isSound} change={() => handleToggle({ name: 'isSound', val: isSound })}>Sound</Switch>
+
+      {#if vibrate}
+        <Switch toggle={isVibrate} change={() => handleToggle({ name: 'isVibrate', val: isVibrate })}>Vibrate</Switch>
+      {/if}
+    </div>
+    <div class="controls">
       <button on:click={() => setState('start')} disabled={!isPause}>
         <i class="fas fa-play" />
       </button>
       <button on:click={() => setState('stop')} disabled={isPause}>
         <i class="fas fa-pause" />
       </button>
-      <button on:click={() => setState('restart')} disabled={secondsLeft === 0}>
+      <button
+        on:click={() => setState('restart')}
+        disabled={secondsLeft !== 0 && isPause === false}>
         <i class="fas fa-redo" />
       </button>
     </div>
